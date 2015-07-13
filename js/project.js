3d44dc740c1deb156a03b714587b1d2ac2d80b1a
katiewelton <katie.welton@gmail.com>
2015-07-13 16:25:38 +0100

    mobile fix
    increase line height and shrink header for mobile

--- js/project.js
+++ js/project.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 // alert("Does it work?");
 
-	$("a.trigger").on("click", function(){
+	$(".trigger").on("click", function(){
 		console.log("clicked");
 		$(".rightColumn").slideToggle(1000);
 		$(".leftColumn").toggleClass("leftSlide");
