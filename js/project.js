fee801884a4fa18088711cac05cd3a3a3e926ae1
katiewelton <katie.welton@gmail.com>
2015-07-13 14:08:04 +0100

    links fix
    all links except blog, projects and PDF now work

--- js/project.js
+++ js/project.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
 // alert("Does it work?");
 
-$("a.trigger").on("click", function(){
-	console.log("clicked");
-	$(".rightColumn").slideToggle(1000);
-	$(".leftColumn").toggleClass("leftSlide");
+	$("a.trigger").on("click", function(){
+		console.log("clicked");
+		$(".rightColumn").slideToggle(1000);
+		$(".leftColumn").toggleClass("leftSlide");
 	});
+	
+	$(".backToTop").on("click", function(){
+		$("html, body").animate({scrollTop: 0}, 1000);
+		return false;
+	});
+
 });
 
